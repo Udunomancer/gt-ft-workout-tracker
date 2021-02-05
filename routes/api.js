b3e45d35a61a665c64fe1a424ca803b1dc1d00e7
Kyle Chriscoe <kyle717@gmail.com>
2021-02-05 14:49:03 -0500

    moved route to api.js file

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/Workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find().then((workout) => {
+        res.json(workout);
+    });
+});
 
 
 
