bb3d1b575bfb90bfd799d1ec89221e7aaeb2229d
Kyle Chriscoe <kyle717@gmail.com>
2021-02-05 14:26:09 -0500

    successfully running seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+// let db = require("../models/workout.js");
+let Workout = require("../models/workout.js")
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -124,8 +125,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
