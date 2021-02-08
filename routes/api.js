8440d34323aff399b4758e9d945d262d34c0470c
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 16:57:19 -0500

    added shells for all api routes

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,25 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+    
+})
+
+router.post("/api/workouts", ({body}, res) => {
+    const workout = new Workout(body);
+    Workout.create(workout)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+})
+
 
 
 module.exports = router;
