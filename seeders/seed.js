c9f0aee49e7764b8bef759ab93dc6d845f661f14
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 21:23:21 -0500

    added connection to seeds

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,9 +2,11 @@ let mongoose = require("mongoose");
 // let db = require("../models/workout.js");
 let Workout = require("../models/workout.js")
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 let workoutSeed = [
