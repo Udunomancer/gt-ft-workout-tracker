1c62676ae59c779c4a55763bab635411de5df320
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 20:28:30 -0500

    updated require request

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find().then((workout) => {
