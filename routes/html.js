bac8500dbb1680542832b6ee93cdb84df9b2f98c
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 22:01:26 -0500

    added comments to HTML routes

--- routes/html.js
+++ routes/html.js
@@ -1,11 +1,15 @@
+// === Importing required dependencies ===
 const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+// === HTML Routes ===
+// --- Get route for the stats page ---
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 })
 
+// --- Get route for the exercise page ---
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 })
