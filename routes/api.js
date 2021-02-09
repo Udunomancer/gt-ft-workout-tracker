e3b4fba0cd28ffc07c262ae171a038c9427e79f5
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 19:18:04 -0500

    added functionality to /api/workouts/range

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,27 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find().then((workout) => {
-        res.json(workout);
-    });
+    // Workout.find()
+    Workout
+    .aggregate([
+        {"$unwind": "$exercises"},
+        {"$addFields": {"totalDuration": {"$sum": "$exercises.duration"}}}
+    ])
+    .sort({day: -1})
+    .then((workouts) => {
+        workouts.splice(7);
+        console.log(workouts);
+    })
+    // Workout
+    // .aggregate
+    // .addFields({
+    //     totalDuration: {$sum: $exercises.duration}
+    // })
+    // .sort({day: -1})
+    // .then((workouts) => {
+    //     workouts.splice(7);
+    //     res.json(workouts);
+    // });
 })
 
 router.post("/api/workouts", ({body}, res) => {
