f8d791fc5d77e107e84ce9599ac9721a1cb0f6db
Kyle Chriscoe <kyle717@gmail.com>
2021-02-08 22:00:04 -0500

    added comments to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,87 +1,70 @@
+// === Import required dependencies ===
 const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
 
+// === API Routes ===
+// --- Get route for all workouts ---
 router.get("/api/workouts", (req, res) => {
+    // Find all workouts
     Workout.find()
-    // Workout
-    // .aggregate([
-    //     {"$unwind": "$exercises"},
-    //     {"$addFields": {"totalDuration": {"$sum": "$exercises.duration"}}},
-    //     {"$group": {
-    //         "_id": "$_id",
-    //         "day": {"$first": "$day"},
-    //         "exercises": {
-    //             "$push": "$exercises"
-    //         },
-    //         "totalDuration": {"$sum": "$totalDuration"}
-    //         }
-    //     }
-    // ])
+    // Then return them as JSON
     .then((workout) => {
         res.json(workout);
+    })
+    // Otherwise return an error
+    .catch((err) => {
+        res.json(err);
     });
 });
 
+// --- Get route for 7 most recent workouts ---
 router.get("/api/workouts/range", (req, res) => {
+    // Find all workouts
     Workout.find()
-    // Workout
-    // .aggregate([
-    //     {"$unwind": "$exercises"},
-    //     {"$addFields": {"totalDuration": {"$sum": "$exercises.duration"}}},
-    //     {"$group": {
-    //         "_id": "$_id",
-    //         "day": {"$first": "$day"},
-    //         "exercises": {
-    //             "$push": "$exercises"
-    //         },
-    //         "totalDuration": {"$sum": "$totalDuration"}
-    //         }
-    //     }
-    // ])
+    // Sort for most recent workouts
     .sort({day: -1})
+    // Limit results to 7
     .limit(7)
-    .sort({day: 1})
+    //  Then return results as JSON
     .then((workouts) => {
-        // if(workouts.length > 7) {
-        //     workouts.splice(7);
-        // }
-        // console.log(workouts);
         res.json(workouts);
     })
-    // Workout
-    // .aggregate
-    // .addFields({
-    //     totalDuration: {$sum: $exercises.duration}
-    // })
-    // .sort({day: -1})
-    // .then((workouts) => {
-    //     workouts.splice(7);
-    //     res.json(workouts);
-    // });
+    // Otherwise return an error
+    .catch((err) => {
+        res.json(err);
+    });
 })
 
+// --- Post route to create a workout ---
 router.post("/api/workouts", ({body}, res) => {
-    console.log(body);
+    // Create a new workout with current date
     Workout.create({day: new Date()})
+    // Then return the created workout
     .then(dbWorkout => {
-        console.log(dbWorkout);
         res.json(dbWorkout);
     })
+    // Otherwise return an error
     .catch(err => {
         res.json(err);
     });
 });
 
+// --- Put route to update a workout with a new exercise ---
 router.put("/api/workouts/:id", (req, res) => {
+    // Get the exercise details from the body
     const exercise = req.body;
+    // Update the workout identified by id, and push the new exercise
     Workout.updateOne(
         {_id: req.params.id}, 
         {$push: {exercises: exercise}}
-    ).then(dbWorkout => {
-        console.log(dbWorkout);
+    )
+    // Then return the updated workout
+    .then(dbWorkout => {
         res.json(dbWorkout);
-    }).catch(err => {
+    })
+    // Otherwise return an error
+    .catch(err => {
         res.json(err);
     })
 })
